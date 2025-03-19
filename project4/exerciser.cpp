da4741f5b8eb2cba05027fa6a85328189a47cd77
Matty-7 <jh730@duke.edu>
2025-03-18 20:21:15 -0400

    add-gitignore

--- project4/exerciser.cpp
+++ project4/exerciser.cpp
@@ -1,26 +1,34 @@
 #include "exerciser.h"
 #include "query_funcs.h"
 
-// This function calls queries for testing.
 void exercise(connection *C)
 {
-  // Query1:
-  query1(C, 1, 35, 40, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-  query1(C, 1, 35, 40, 1, 15, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+  // Query1: 
+  // test players with RPG between 6-10
+  query1(C, 0, 0, 0, 0, 0, 0, 1, 6, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+  // test players with APG > 5 and SPG > 1.5
+  query1(C, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 5, 10, 1, 1.5, 3.0, 0, 0, 0);
+  // test players with PPG>18, RPG>8
+  query1(C, 0, 0, 0, 1, 18, 25, 1, 8, 15, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-  // Query2:
-  query2(C, "Green");
-  query2(C, "Orange");
+  // Query2: 
+  query2(C, "Red");
+  query2(C, "DarkBlue");
+  query2(C, "Maroon");
 
-  // Query3:
-  query3(C, "Duke");
-  query3(C, "UNC");
+  // Query3: 
+  query3(C, "Miami");
+  query3(C, "Virginia");
+  query3(C, "Louisville");
 
-  // Query4:
-  query4(C, "MA", "Maroon");
-  query4(C, "NC", "DarkBlue");
+  // Query4: 
+  query4(C, "FL", "Orange");   
+  query4(C, "VA", "Blue");     
+  query4(C, "NC", "Red");      
 
-  // Query5:
-  query5(C, 13);
-  query5(C, 10);
+  // Query5: 
+  // teams with more than 8 wins
+  query5(C, 8);   
+  // teams with more than 12 wins
+  query5(C, 12);  
 }
